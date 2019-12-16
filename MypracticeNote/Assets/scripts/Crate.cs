7a64afc3c6d7d7a10b011be4c289e8d656782706
Themyscira <takeda.syouhei@gmail.com>
2019-12-16 19:26:49 +0900

    Make BrakeableCrate
    when you make crate's debris ,Watch this commitment.

---
+++ MypracticeNote/Assets/scripts/Crate.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Crate : MonoBehaviour
+{
+    public GameObject frature_crate_;
+    public GameObject explotion_effect_;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+           
+            Instantiate(explotion_effect_, transform.position, Quaternion.identity);
+
+            ///// GetComponentInChildren
+            ///  FCO is crate for entering Instanstiated objects. 
+            ///  https://docs.unity3d.com/ja/current/ScriptReference/Component.GetComponentInChildren.html
+            ///  ""
+
+            GameObject fractuated_crate_obj = Instantiate(frature_crate_, transform.position, Quaternion.identity) as GameObject;
+            Rigidbody[] allrigidbodies_ = fractuated_crate_obj.GetComponentInChildren<Rigidbody[]>();
+            if (allrigidbodies_.Length > 0)
+            {
+                foreach(var body in allrigidbodies_)
+                {
+                    body.AddExplosionForce(500, transform.position,1);
+                }
+            }
+
+            Destroy(this.gameObject);
+        }
+    }
+}
