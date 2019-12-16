ed252dfbf9c5e0e9286569f530e9297652f2fefa
Themyscira <takeda.syouhei@gmail.com>
2019-12-16 14:20:34 +0900

    CollectibleGameobjects

---
+++ MypracticeNote/Assets/Scropts/Collectable.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collectable : MonoBehaviour
+{
+
+    
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        ////
+        ///Don't forget Triger enter of Check!!
+        ///
+        if (other.tag == "Player")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
