be65ad815bbc47a17f2450788b9a4f878db38430
Themyscira <takeda.syouhei@gmail.com>
2019-12-16 14:41:22 +0900

    Pause system

---
+++ MypracticeNote/Assets/Scropts/Pause.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pause : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    /// <summary>
+    /// When you show objects like Pause ,Please compound Camera
+    /// stop Time.timeScale = 0;
+    /// slow Time.timeScale = 0.25f;
+    /// restart Time.timeScale = 1;
+    /// </summary>
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            //Stop Movement
+            Time.timeScale = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            //Restart movement
+            Time.timeScale = 1;
+
+        }
+
+    }
+}
