6aac4561973c89e44a2546044f7fc225f6c5e372
Themyscira <takeda.syouhei@gmail.com>
2019-12-16 13:31:34 +0900

    transform.positon

--- MypracticeNote/Assets/Scropts/Player.cs
+++ MypracticeNote/Assets/Scropts/Player.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Player : MonoBehaviour
 {
+    public Vector3 start_potison;
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = new Vector3(0,0,0);
     }
 
     // Update is called once per frame
